3c6267f4fe6b928254697c75c82491fc40c2b525
Giso Pillar <GisoPillar@yahoo.de>
2015-06-05 16:27:02 +0200

    Added rim to arrow

--- src/de/noah/naime/compars/ArrowRenderer.java
+++ src/de/noah/naime/compars/ArrowRenderer.java
@@ -1,9 +1,9 @@
 /****************************** CompARs ******************************
-* TODO:	OpenGlES Chapter 6 / AirHockey3D
-* 	Change rotation so arrow rotates around middle point
+* TODO:	OpenGlES Chapter 8.6 / AirHockeyWithImprovedMallets
+* 	Add black edges (GL_LINE) to arrow
 * 	Remove menu bar
 *	Description, Author, etc
-* 	Change to british english
+* 	Change to British English
 *********************************************************************/
 package de.noah.naime.compars;
 
@@ -11,6 +11,7 @@ import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
 import static android.opengl.GLES20.GL_FLOAT;
 import static android.opengl.GLES20.GL_TRIANGLES;
 import static android.opengl.GLES20.GL_TRIANGLE_FAN;
+import static android.opengl.GLES20.GL_TRIANGLE_STRIP;
 import static android.opengl.GLES20.glClear;
 import static android.opengl.GLES20.glClearColor;
 import static android.opengl.GLES20.glDrawArrays;
@@ -42,8 +43,7 @@ public class ArrowRenderer implements Renderer {
 	
 	private static final String A_COLOR = "a_Color";
 	private static final String A_POSITION = "a_Position";  
-//	private static final int POSITION_COMPONENT_COUNT = 4; 
-	private static final int POSITION_COMPONENT_COUNT = 2;
+	private static final int POSITION_COMPONENT_COUNT = 3;
 	private static final int COLOR_COMPONENT_COUNT = 3;
 	private static final int BYTES_PER_FLOAT = 4;
 	// Skip over the colour bytes for the current vertex
@@ -70,36 +70,68 @@ public class ArrowRenderer implements Renderer {
 		this.context = context;
 		
 		float[] arrowVertices = {
-			// Order of coordinates: X, Y, Z, W, R, G, B
+			// Order of coordinates: X, Y, Z, R, G, B
+			
+			// Rim - Head Bottom left (Triangle Strip)
+			-0.25f,	0.1f,	0,	1f,	0.3f,	0.3f,	// Top right (Z)
+			-0.25f,	0.1f,	-0.2f,	1f,	0.3f,	0.3f,	// Bottom right (Z)
+			-0.5f,	0.1f,	0,	1f,	0.3f,	0.3f,	// Top left (Z)
+			-0.5f,	0.1f,	-0.2f,	1f,	0.3f,	0.3f,	// Bottom left (Z)
+			
+			// Rim - Head Bottom right
+			0.25f,	0.1f,	0,	1f,	0.3f,	0.3f,	// Top right (Z)
+			0.25f,	0.1f,	-0.2f,	1f,	0.3f,	0.3f,	// Bottom right (Z)
+			0.5f,	0.1f,	0,	1f,	0.3f,	0.3f,	// Top left (Z)
+			0.5f,	0.1f,	-0.2f,	1f,	0.3f,	0.3f,	// Bottom left (Z)
+			
+			// Rim - Head Top left
+			-0.5f,	0.1f,	0,	1f,	0.3f,	0.3f,	// Top left (Z)
+			-0.5f,	0.1f,	-0.2f,	1f,	0.3f,	0.3f,	// Bottom left (Z)
+			0,	0.5f,	0,	1f,	0.3f,	0.3f,	// Top right (Z)
+			0,	0.5f,	-0.2f,	1f,	0.3f,	0.3f,	// Bottom right (Z)
+			
+			// Rim - Head Top right
+			0.5f,	0.1f,	0,	1f,	0.3f,	0.3f,	// Top left (Z)
+			0.5f,	0.1f,	-0.2f,	1f,	0.3f,	0.3f,	// Bottom left (Z)
+			0,	0.5f,	0,	1f,	0.3f,	0.3f,	// Top right (Z)
+			0,	0.5f,	-0.2f,	1f,	0.3f,	0.3f,	// Bottom right (Z)
+			
+			// Rim - Tail left side (Triangle Strip)
+			-0.25f,	-0.5f,	0,	0.1f,	1f,	0.1f,	// Top front (Z)
+			-0.25f,	-0.5f,	-0.2f,	0.1f,	1f,	0.1f,	// Bottom front (Z)
+			-0.25f,	-0.1f,	0,	0.1f,	1f,	0.1f,	// Top middle (Z)
+			-0.25f,	-0.1f,	-0.2f,	0.1f,	1f,	0.1f,	// Bottom middle (Z)
+			-0.25f,	0.1f,	0,	1f,	0.3f,	0.3f,	// Top back (Z)
+			-0.25f,	0.1f,	-0.2f,	1f,	0.3f,	0.3f,	// Bottom back (Z)
+			
+			// Rim - Tail right side (Triangle Strip)
+			0.25f,	-0.5f,	0,	0.1f,	1f,	0.1f,	// Top front (Z)
+			0.25f,	-0.5f,	-0.2f,	0.1f,	1f,	0.1f,	// Bottom front (Z)
+			0.25f,	-0.1f,	0,	0.1f,	1f,	0.1f,	// Top middle (Z)
+			0.25f,	-0.1f,	-0.2f,	0.1f,	1f,	0.1f,	// Bottom middle (Z)
+			0.25f,	0.1f,	0,	1f,	0.3f,	0.3f,	// Top back (Z)
+			0.25f,	0.1f,	-0.2f,	1f,	0.3f,	0.3f,	// Bottom back (Z)
+			
+			// Rim - Tail end (Triangle Strip)
+			-0.25f,	-0.5f,	0,	0.1f,	1f,	0.1f,	// Top left (Z)
+			-0.25f,	-0.5f,	-0.2f,	0.1f,	1f,	0.1f,	// Bottom left (Z)
+			0.25f,	-0.5f,	0,	0.1f,	1f,	0.1f,	// Top right (Z)
+			0.25f,	-0.5f,	-0.2f,	0.1f,	1f,	0.1f,	// Bottom right (Z)
 			
 			// Head
-//			0,	0.5f,	-0.5f,	2f,	1f,	0.2f,	0.2f,	// Top
-//			-0.5f,	0.1f,	-0.5f,	1.6f,	1f,	0.2f,	0.2f,	// Left
-//			0.5f,	0.1f,	-0.5f,	1.6f,	1f,	0.2f,	0.2f,	// Right
-//			
-//			// Tail - Triangle Fan
-//			0,	-0.1f,	-0.5f,	1.4f,	0.2f,	1f,	0.2f,	// Middle
-//			-0.25f,	-0.5f,	-0.5f,	1f,	0.2f,	1f,	0.2f,	// Bottom left
-//			0.25f,	-0.5f,	-0.5f,	1f,	0.2f,	1f,	0.2f,	// Bottom right
-//			0.25f,	0.1f,	-0.5f,	1.6f,	1f,	0.2f,	0.2f,	// Top right
-//			-0.25f,	0.1f,	-0.5f,	1.6f,	1f,	0.2f,	0.2f,	// Top left
-//			-0.25f,	-0.5f,	-0.5f,	1f,	0.2f,	1f,	0.2f,	// Bottom left
+			0,	0.5f,	0,	1f,	0.3f,	0.3f,	// Top
+			-0.5f,	0.1f,	0,	1f,	0.3f,	0.3f,	// Left
+			0.5f,	0.1f,	0,	1f,	0.3f,	0.3f,	// Right
 			
-			// Order of coordinates: X, Y, R, G, B
-			// Head
-			0,	0.5f,	1f,	0.2f,	0.2f,	// Top
-			-0.5f,	0.1f,	1f,	0.2f,	0.2f,	// Left
-			0.5f,	0.1f,	1f,	0.2f,	0.2f,	// Right
-			
-			// Tail - Triangle Fan
-			0,	-0.1f,	0.2f,	1f,	0.2f,	// Middle
-			-0.25f,	-0.5f,	0.2f,	1f,	0.2f,	// Bottom left
-			0.25f,	-0.5f,	0.2f,	1f,	0.2f,	// Bottom right
-			0.25f,	-0.1f,	0.2f,	1f,	0.2f,	// Middle right
-			0.25f,	0.1f,	1f,	0.2f,	0.2f,	// Top right
-			-0.25f,	0.1f,	1f,	0.2f,	0.2f,	// Top left
-			-0.25f,	-0.1f,	0.2f,	1f,	0.2f,	// Middle left
-			-0.25f,	-0.5f,	0.2f,	1f,	0.2f,	// Bottom left
+			// Tail (Triangle Fan)
+			0,	-0.1f,	0,	0.3f,	1f,	0.3f,	// Middle
+			-0.25f,	-0.5f,	0,	0.3f,	1f,	0.3f,	// Bottom left
+			0.25f,	-0.5f,	0,	0.3f,	1f,	0.3f,	// Bottom right
+			0.25f,	-0.1f,	0,	0.3f,	1f,	0.3f,	// Middle right
+			0.25f,	0.1f,	0,	1f,	0.3f,	0.3f,	// Top right
+			-0.25f,	0.1f,	0,	1f,	0.3f,	0.3f,	// Top left
+			-0.25f,	-0.1f,	0,	0.3f,	1f,	0.3f,	// Middle left
+			-0.25f,	-0.5f,	0,	0.3f,	1f,	0.3f,	// Bottom left
 		};
 		
 		vertexData = ByteBuffer
@@ -181,18 +213,6 @@ public class ArrowRenderer implements Renderer {
 		final float[] temp = new float[16];
 		multiplyMM(temp, 0, projectionMatrix, 0, modelMatrix, 0);
 		System.arraycopy(temp, 0, projectionMatrix, 0, temp.length);
-
-//		final float aspectRatio = width > height ? 
-//			(float) width / (float) height : 
-//			(float) height / (float) width;
-
-//		if (width > height) {
-//			// Landscape
-//			orthoM(projectionMatrix, 0, -aspectRatio, aspectRatio, -1f, 1f, -1f, 1f);
-//		} else {
-//			// Portrait or square
-//			orthoM(projectionMatrix, 0, -1f, 1f, -aspectRatio, aspectRatio, -1f, 1f);
-//		}   
 	}
 
 	// Draw a frame
@@ -205,13 +225,21 @@ public class ArrowRenderer implements Renderer {
 		// Add orthographic projection
 		glUniformMatrix4fv(uMatrixLocation, 1, false, projectionMatrix, 0);
 		
-		// Draw Arrow head
+		// Draw rim first
+		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);	// Head Bottom left
+		glDrawArrays(GL_TRIANGLE_STRIP, 4, 4);	// Head Bottom right
+		glDrawArrays(GL_TRIANGLE_STRIP, 8, 4);	// Head Top left
+		glDrawArrays(GL_TRIANGLE_STRIP, 12, 4);	// Head Top left
+		glDrawArrays(GL_TRIANGLE_STRIP, 16, 6);	// Tail left side
+		glDrawArrays(GL_TRIANGLE_STRIP, 22, 6);	// Tail right side
+		glDrawArrays(GL_TRIANGLE_STRIP, 28, 4);	// Tail end
+		
+		// Draw arrow head
 		// Draw triangles, start at beginning of vertex array, read in three vertices
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawArrays(GL_TRIANGLES, 32, 3);
 		
-		// Draw Arrow tail
+		// Draw arrow tail
 		// Draw triangle fan, start after first object, read in eight vertices
-		glDrawArrays(GL_TRIANGLE_FAN, 3, 8);
-		
+		glDrawArrays(GL_TRIANGLE_FAN, 35, 8);
 	}
 }
